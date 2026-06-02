ac20067af6e7a7e804b46ea9524c6b3f1989cde2
Julia Lopes da Rocha <jlrocha@inoa.com.br>
2026-06-02 17:18:24 -0300

    + new class: Carteira

---
+++ Bolsa/Models/Carteira.cs
@@ -0,0 +1,27 @@
+﻿namespace Bolsa.Models {
+    public class Carteira(Usuario usuario) {
+        public Usuario Usuario { get; } = usuario;
+
+        // Dicionário para guardar: <Ticker da Ação, Quantidade Disponível>
+        public Dictionary<string, int> Ativos { get; } = [];
+
+        public void AdicionarAtivo(string ticker, int quantidade) {
+            ticker = ticker.ToUpper();
+            if (Ativos.ContainsKey(ticker))
+                Ativos[ticker] += quantidade;
+            else
+                Ativos[ticker] = quantidade;
+        }
+
+        public void RemoverAtivo(string ticker, int quantidade) {
+            ticker = ticker.ToUpper();
+            if (Ativos.ContainsKey(ticker) && Ativos[ticker] >= quantidade) {
+                Ativos[ticker] -= quantidade;
+                if (Ativos[ticker] == 0) Ativos.Remove(ticker);
+            } else {
+                throw new InvalidOperationException("Quantidade insuficiente de ativos para venda");
+            }
+        }
+
+    }
+}
