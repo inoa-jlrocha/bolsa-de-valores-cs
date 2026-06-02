46482bb291dbd1a98e4d22c902fde92d349991fa
Julia Lopes da Rocha <jlrocha@inoa.com.br>
2026-06-02 16:40:32 -0300

    + classes: ativo properties setup

---
+++ Bolsa/Models/Ativo.cs
@@ -0,0 +1,9 @@
+﻿namespace Bolsa.Models {
+    internal class Ativo(string ticker, string nomeEmpresa, decimal precoInicial) {
+
+        // Propriedades do Ativo
+        public string Ticker { get; set; } = ticker.ToUpper();
+        public string NomeEmpresa { get; set; } = nomeEmpresa;
+        public decimal PrecoInicial { get; set; } = precoInicial;
+    }
+}
---
+++ Bolsa/Models/Ordem.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bolsa.Models {
+    internal class Ordem {
+    }
+}
